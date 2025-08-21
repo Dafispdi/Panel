aeea7497e492e7df2f58dec722abab2a43ea5aa4
Dafispdi <137497706+Dafispdi@users.noreply.github.com>
2025-08-21 19:08:30 +0700

    Delete api.js

--- api.js
+++
@@ -1,5 +0,0 @@
-async function handleBugSend(chatId, selectedBug) {
-  const res = await fetch(`https://kyxzan-ganzz.404-eror.systems/kyxsan?chatId=${encodeURIComponent(chatId)}&type=${selectedBug}`);
-  if (!res.ok) throw new Error("Gagal menghubungi server");
-  return await res.json();
-}
